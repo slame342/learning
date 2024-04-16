ec3d632e2313160752caf5ead244a536e6470423
slame342 <105666656+slame342@users.noreply.github.com>
2024-04-16 14:53:27 +0500

    new

--- calculate.py
+++ calculate.py
@@ -1,3 +1,5 @@
+# доделать выделение корня!!!
+
 def calc(number1, number2, operations):
     if operations == "+":
         return number1 + number2
@@ -12,12 +14,18 @@ def calc(number1, number2, operations):
             return number1 / number2
         else:
             print('Error!')
-            return 0
+            return print("на нуль делить нельзя!!!")
     if operations == "//":
         if number2 != 0:
             return number1 // number2
         else:
             print('Error!')
+            return 0
+    if operations == "%":
+        if number2 != 0:
+            return number1 % number2
+        else:
+            print('Error! вы ввели нуль')
             return 0
 
 
@@ -33,4 +41,5 @@ result = calc(33, 0, "/")
 print(result)
 result = calc(33, 2, "//")
 print(result)
-
+result = calc(33, 0, "%")
+print(result)
